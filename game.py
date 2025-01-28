ecba0f474b95e328384b1be45efaa84153220d15
Utsav Belbase <utsavbelbase@mail.weber.edu>
2025-01-28 12:06:22 -0500

    ADD: Alternate string with f string starting area text

--- game.py
+++ game.py
@@ -1,4 +1,26 @@
-'''adventure game 
-author = utsav'''
+'''
+DOCSTRING
+adventure game 
+author = utsav
+'''
 # Welcome message and introduction
 print("welcome to the adventure game")
+
+player_name= input("What is your name adventurer??: ")
+
+
+# concatination of string 
+print("welcome, " + player_name +  "!Your journey begins here now.")
+
+#Using f-strings
+print(f" welcome {player_name} Now you are in a long journey")
+
+#Describe the starting area
+
+Starting_area = """
+You find yourslef in a dark forest
+The sound of rustling leaves fills the air
+A faint path lies ahead, leading deeper into the 
+Unknown.....
+"""
+print(Starting_area)
